611f78f92716fd3b6ca347241741d49ee02bdb85
Brandon Corey <bcorey85@gmail.com>
2019-12-02 14:40:14 -0600

    init commit

--- src/Form.js
+++ src/Form.js
@@ -1,61 +1,95 @@
-import React, { Component } from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import withStyles from "@material-ui/core/styles/withStyles";
-import styles from "./styles/FormStyles";
+import React, { Component } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import withStyles from '@material-ui/core/styles/withStyles';
+import styles from './styles/FormStyles';
+import { LanguageContext } from './contexts/LanguageContext';
+
+const words = {
+	english: {
+		email: 'Email',
+		password: 'Password',
+		signIn: 'Sign In',
+		remember: 'Remember Me'
+	},
+	french: {
+		email: 'Adresse Electronique',
+		password: 'Mot de Passe',
+		signIn: 'Se Connecter',
+		remember: 'Souviens-toi De Moi'
+	},
+	spanish: {
+		email: 'Correo Electronico',
+		password: 'Contrasena',
+		signIn: 'Registrarse',
+		remember: 'Recuerdame'
+	}
+};
 
 class Form extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography variant='h5'>Sign In</Typography>
-          <Select value='english'>
-            <MenuItem value='english'>English</MenuItem>
-            <MenuItem value='french'>French</MenuItem>
-            <MenuItem value='spanish'>Spanish</MenuItem>
-          </Select>
-          <form className={classes.form}>
-            <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='email'>Email</InputLabel>
-              <Input id='email' name='email' autoFocus />
-            </FormControl>
-            <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='password'>Password</InputLabel>
-              <Input id='password' name='password' autoFocus />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox color='primary' />}
-              label='Remember Me'
-            />
-            <Button
-              variant='contained'
-              type='submit'
-              fullWidth
-              color='primary'
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    );
-  }
+	static contextType = LanguageContext;
+	render() {
+		const { language, changeLanguage } = this.context;
+		const { classes } = this.props;
+		const { main, paper, avatar, form, submit } = classes;
+		const { email, signIn, remember, password } = words[language];
+		return (
+			<main className={main}>
+				<Paper className={paper}>
+					<Avatar className={avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography variant='h5'>{signIn}</Typography>
+					<Select value={language} onChange={changeLanguage}>
+						<MenuItem value='english'>English</MenuItem>
+						<MenuItem value='french'>French</MenuItem>
+						<MenuItem value='spanish'>Spanish</MenuItem>
+					</Select>
+					<form className={form}>
+						<FormControl margin='normal' required fullWidth>
+							<InputLabel htmlFor='email'>{email}</InputLabel>
+							<Input id='email' name='email' autoFocus />
+						</FormControl>
+						<FormControl margin='normal' required fullWidth>
+							<InputLabel htmlFor='password'>
+								{password}
+							</InputLabel>
+							<Input
+								id='password'
+								name='password'
+								type='password'
+								autoFocus
+							/>
+						</FormControl>
+						<FormControlLabel
+							control={<Checkbox color='primary' />}
+							label={remember}
+						/>
+						<Button
+							variant='contained'
+							type='submit'
+							fullWidth
+							color='primary'
+							className={submit}>
+							{signIn}
+						</Button>
+					</form>
+				</Paper>
+			</main>
+		);
+	}
 }
+
 export default withStyles(styles)(Form);
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,45 +1,76 @@
-import React, { Component } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
-import Switch from "@material-ui/core/Switch";
-import { withStyles } from "@material-ui/core/styles";
-import styles from "./styles/NavBarStyles";
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core';
+import styles from './styles/NavBarStyles';
+import { ThemeContext } from './contexts/ThemeContext';
+import { withLanguageContext } from './contexts/LanguageContext';
 
+const content = {
+	english: {
+		search: 'Search'
+	},
+	french: {
+		search: 'Chercher'
+	},
+	spanish: {
+		search: 'Buscar'
+	}
+};
 class Navbar extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <AppBar position='static' color='primary'>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color='inherit'>
-              <span>🇫🇷</span>
-            </IconButton>
-            <Typography className={classes.title} variant='h6' color='inherit'>
-              App Title
-            </Typography>
-            <Switch />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder='Search...'
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+	static contextType = ThemeContext;
+
+	render() {
+		const { isDarkMode, toggleDarkMode } = this.context;
+		const { classes } = this.props;
+		const { language } = this.props.languageContext;
+		const searchTxt = content[language].search;
+
+		const {
+			root,
+			menuButton,
+			title,
+			grow,
+			search,
+			searchIcon,
+			inputInput
+		} = classes;
+		return (
+			<div className={root}>
+				<AppBar
+					position='static'
+					color={isDarkMode ? 'default' : 'primary'}>
+					<Toolbar>
+						<Typography
+							className={title}
+							variant='h6'
+							color='inherit'>
+							AppTitle
+						</Typography>
+						<Switch onClick={toggleDarkMode} />
+						<div className={grow} />
+						<div className={search}>
+							<div className={searchIcon}>
+								<SearchIcon />
+							</div>
+							<InputBase
+								placeholder={`${searchTxt}...`}
+								classes={{
+									root: root,
+									input: inputInput
+								}}
+							/>
+						</div>
+					</Toolbar>
+				</AppBar>
+			</div>
+		);
+	}
 }
-export default withStyles(styles)(Navbar);
+
+export default withLanguageContext(withStyles(styles)(Navbar));
